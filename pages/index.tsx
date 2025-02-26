7d36f7a4c398cdf5a55f7919d61edf62d20ed420
BharataITTeam <bharataitteam@gmail.com>
2025-02-26 13:59:32 +0700

    infra: added chakra-ui

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Container>
+      <Text>Hello, Next.js!</Text>
+    </Container>
+  )
+}
